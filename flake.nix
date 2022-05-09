33dbb8575826b5e06502e264dde2fed857163d9a
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:30 -0300

    chore: re index bcra

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bcra-master".dir   = "master";
-  inputs."bcra-master".owner = "nim-nix-pkgs";
-  inputs."bcra-master".ref   = "master";
-  inputs."bcra-master".repo  = "bcra";
-  inputs."bcra-master".type  = "github";
-  inputs."bcra-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bcra-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
